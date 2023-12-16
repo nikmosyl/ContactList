1ad6c723b33daefb709ea7e0a02ccefdab5b1782
ns.moskalev <nikmosyl@gmail.com>
2023-12-16 23:06:23 +0400

    implemented person list screen

---
+++ ContactList/Models/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by nikita on 16.12.23.
+//
+
+import Foundation
---
+++ ContactList/ViewControllers/ContactListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ContactListViewController.swift
+//  ContactList
+//
+//  Created by nikita on 16.12.23.
+//
+
+import UIKit
+
+final class ContactListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
